cbbe76d58fc53c1cbc2802eae20793d0c2b2e288
jugurj <juriyunna27@gmail.com>
2019-06-21 21:16:27 +0300

    Updated gitignore with new or expected packages/file extensions. Connected cookie/json parsers and mongoDB itself to the server. Enabled dotenv to store local environment variables and using them to declare actual database link.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE,  { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-    console.log(`Server running at port:${port}`);
+    console.log(`Server running at port: ${port}`);
 })
