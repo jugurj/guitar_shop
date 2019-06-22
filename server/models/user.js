d4edef7874285e662a382fdcd932e5e857fbaccd
jugurj <juriyunna27@gmail.com>
2019-06-22 18:59:52 +0300

    Refactored password hashing to be partically in ES6 instead of ES5.

--- server/models/user.js
+++ server/models/user.js
@@ -41,19 +41,20 @@ const userSchema = mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function (next) {
-    var user = this;
+userSchema.pre('save', function(next) {
+    let user = this;
 
     if (user.isModified('password')) {
-        bcrypt.genSalt(SALT_INDEX, function (err, salt) {
+        bcrypt.genSalt(SALT_INDEX, (err, salt) => {
             if (err) return next(err);
-            bcrypt.hash(user.password, salt, function (err, hash) {
+            bcrypt.hash(user.password, salt, (err, hash) => {
                 if (err) return next(err);
                 user.password = hash;
                 next();
-            });
-        });
+            })
+        })
     } else { next() }
+    
 });
 
 const User = mongoose.model('User', userSchema);
